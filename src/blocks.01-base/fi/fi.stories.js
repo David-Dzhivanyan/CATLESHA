8946542f834fd749ac80b931472aa8c692312057
AlexanderShchedrin <shchedrin@intervolga.ru>
2023-10-23 16:27:34 +0300

    [icon-font] - Добавил сборщик иконок в проект

--- src/blocks.01-base/fi/fi.stories.js
+++
@@ -1,6 +0,0 @@
-const {getExample} = require('helpers');
-module.exports = Object.assign({
-  default: {
-    title: 'Blocks/fi',
-  },
-}, getExample(require.context('!!babel-loader!./', true, /\.tmpl-specs\/(.*).bemjson\.js$/)));
