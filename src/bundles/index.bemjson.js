3b3a98d12713db30b633d4a725d11b30b201598a
AlexanderShchedrin <shchedrin@intervolga.ru>
2023-10-23 15:51:30 +0300

    [image-size] - Изменил расчёт image_size

--- src/bundles/index.bemjson.js
+++ src/bundles/index.bemjson.js
@@ -1,11 +1,13 @@
 module.exports = {
-    block: 'page',
-    title: 'Пустая',
-    content: [
-        require('./common/header.bemjson'),
-        {cls: 'container', content: [
-                {block: 'img', mods: {lazy: true}, src: 'http://place-hold.it/1200x200'},
-            ]},
-        require('./common/footer.bemjson'),
+  block: 'page',
+  title: 'Пустая',
+  content: [
+    require('./common/header.bemjson'),
+    {cls: 'container', content: [
+        {block: 'image', mods: {size: '1200x200'}, cls: 'text-center align-middle', content: [
+            {block: 'img', attrs: {lazy: true}, src: 'https://placehold.co/1200x200' },
+          ]},
+      ]},
+    require('./common/footer.bemjson'),
     ],
 };
