33a312615a1807961bba441c4080ef92c550b0d5
Dzhivanyan <d.david@intervolga.ru>
2024-06-28 17:03:04 +0300

    [develop] - fix

--- src/blocks.04-project/about/about.tmpl-specs/base.bemjson.js
+++ src/blocks.04-project/about/about.tmpl-specs/base.bemjson.js
@@ -15,7 +15,7 @@ module.exports = ({cls} = {}) => [
                   {block: 'img', src: './images/about/lesha.png'},
                   {elem: 'description', content: [
                       {elem: 'text', content: [
-                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in vo',
+                          'Meet Lesha, the cat who\'s taking the internet by storm! This adorable furball has not only melted hearts on TikTok but also landed a starring role in a Netflix movie. Lesha might be a bit of a messy mischief-maker (his owner claims he\'s the dirtiest cat ever!), but his playful energy and undeniable cuteness win everyone over. Lesha\'s even inspired his own meme token on the ton blockchain - talk about pawsome fame!',
                         ]},
                       {elem: 'buttons', content: [
                           {block: 'btn', cls: 'btn-yellow', attrs: {href: 'https://dedust.io/swap/TON/EQA5-G7djF86kQlspnEu4RI2HN-zgu81DcE_ap0yuA84fa1x?amount=1000000000'}, content: 'buy now'},
