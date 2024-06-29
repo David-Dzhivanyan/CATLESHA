4d13b408947ec4dfb928ee3293333828a520ab4e
Dzhivanyan <d.david@intervolga.ru>
2024-06-29 20:24:50 +0300

    [develop] - money

--- src/blocks.04-project/copy/copy.tmpl-specs/base.bemjson.js
+++ src/blocks.04-project/copy/copy.tmpl-specs/base.bemjson.js
@@ -9,10 +9,13 @@ module.exports = ({cls} = {}) => [
             ]},
           {cls: 'text-uppercase', content: 'contract address'},
           {elem: 'copy', content: [
-              {elem: 'copy-text', cls: 'text-uppercase', content: 'EQAK2J1WU9AuVYR2X_CwL_Tg_DfjFQf8LaRGN2kTLLNPV1Ww'},
+              {elem: 'copy-text', cls: 'text-no-wrap text-uppercase', content: 'EQA5-G7djF86kQlspnEu4RI2HN-zgu81DcE_ap0yuA84fa1x'},
               {block: 'fi', cls: 'd-none d-sm-block', mods: {icon: 'copy'}}
             ]},
           {block: 'btn', cls: 'btn-yellow text-uppercase', attrs: {href: 'https://t.me/catleshaton'}, content: 'JOIN OUR COMMUNITY TELEGRAM!'}
-        ]}
+        ]},
+      {block: 'img', cls: 'copy__money m1', src: './images/logo/money.png'},
+      {block: 'img', cls: 'copy__money m2', src: './images/logo/money.png'},
+      {block: 'img', cls: 'copy__money m3', src: './images/logo/money.png'},
     ]}
 ];
--- src/blocks.04-project/logo/logo.tmpl-specs/base.bemjson.js
+++ src/blocks.04-project/logo/logo.tmpl-specs/base.bemjson.js
@@ -1,5 +1,10 @@
 module.exports = ({cls} = {}) => [
   {block: 'logo', cls, content: [
-      {block: 'img', src: './images/logo/logo-bg.png'}
+      {block: 'img', cls: 'd-block d-md-none', src: './images/logo/logo-bg.png'},
+      {block: 'img', cls: 'd-none d-md-block', src: './images/logo/bg.png'},
+      {block: 'img', cls: 'logo__money m1', src: './images/logo/money.png'},
+      {block: 'img', cls: 'logo__money m2', src: './images/logo/money.png'},
+      {block: 'img', cls: 'logo__money m3', src: './images/logo/money.png'},
+      {block: 'img', cls: 'logo__money m4', src: './images/logo/money.png'},
     ]}
 ];
